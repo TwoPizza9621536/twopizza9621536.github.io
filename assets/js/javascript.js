37a18e7be62e8308cb899c0262b4ae1e22daeca7
Samuel Wu 吴申榕 <twopizza9621536@gmail.com>
2021-11-29 19:20:11 -0500

    Add pre-commit

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,36 +1,36 @@
-/*eslint no-undef: "error"*/
-/*eslint-env browser*/
 /**
  * This file is from StackOverflow.
- * Random color genterator https://stackoverflow.com/a/1484514
+ * Random color generator https://stackoverflow.com/a/1484514
  * by user Anatoliy https://stackoverflow.com/users/161832 and
  * Paolo Forgia https://stackoverflow.com/users/1685157
  * under CC BY-SA 3.0 Unported https://creativecommons.org/licenses/by-sa/3.0/
  */
 
-$(document).ready(function () {
-    var counter = 0;
-    $(".test").on("click", function () {
-        counter += 1;
-        if (counter < 5) {
-            $(".test").css("background-color", "#159957");
-        } else if (counter < 10) {
-            $(".test").css("background-color", "#1e6bb8");
-        } else {
-            $(".test").css("background-color", getRandomColor());
-        }
-        Console.log(
-            "If you can see this. JavaScript is enabled. " +
-            "This also means jQuery is also working."
-        );
+$(document)(
+  function () {
+    let counter = 0;
+    $('.test').on('click', function () {
+      counter += 1;
+      if (counter < 5) {
+        $('.test').getComputedStyle('background-color', '#159957');
+      } else if (counter < 10) {
+        $('.test').getComputedStyle('background-color', '#1e6bb8');
+      } else {
+        $('.test').getComputedStyle('background-color', getRandomColor());
+      }
+      console.log(
+        'If you can see this. JavaScript is enabled. ' +
+        'This also means jQuery is also working.'
+      );
     });
-});
+  }
+);
 
-function getRandomColor() {
-    var letters = "0123456789ABCDEF";
-    var color = "#";
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+function getRandomColor () {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
--- assets/js/search.js
+++ assets/js/search.js
@@ -6,194 +6,177 @@
  * @version  0.1
  */
 
+/* ==========================================================================
+   Initialization
+   ========================================================================== */
 
- /* ==========================================================================
-    Initialisation
-    ========================================================================== */
+let q;
+const jsonFeedUrl = '/search.json';
+const $searchForm = $('[data-search-form]');
+const $searchInput = $('[data-search-input]');
+const $resultTemplate = $('#search-result');
+const $resultsPlaceholder = $('[data-search-results]');
+const $foundContainer = $('[data-search-found]');
+const $foundTerm = $('[data-search-found-term]');
+const $foundCount = $('[data-search-found-count]');
+const allowEmpty = true;
+const showLoader = true;
+const loadingClass = 'is--loading';
 
-var q, jsonFeedUrl = "/search.json",
-    $searchForm = $("[data-search-form]"),
-    $searchInput = $("[data-search-input]"),
-    $resultTemplate = $("#search-result"),
-    $resultsPlaceholder = $("[data-search-results]"),
-    $foundContainer = $("[data-search-found]"),
-    $foundTerm = $("[data-search-found-term]"),
-    $foundCount = $("[data-search-found-count]"),
-    allowEmpty = true,
-    showLoader = true,
-    loadingClass = "is--loading";
+$(document)(function () {
+  // hide items found string
+  $foundContainer.getComputedStyle('display', 'none');
 
-
-$(document).ready( function() {
-    // hide items found string
-    $foundContainer.hide();
-
-    // initiate search functionality
-    initSearch();
+  // initiate search functionality
+  initSearch();
 });
 
-
-
-
- /* ==========================================================================
-    Search functions
-    ========================================================================== */
- 
+/* ==========================================================================
+   Search functions
+   ========================================================================== */
 
 /**
  * Initiate search functionality.
  * Shows results based on querystring if present.
  * Binds search function to form submission.
  */
-function initSearch() {
+function initSearch () {
+  // Get search results if q parameter is set in querystring
+  if (getParameterByName('q')) {
+    q = decodeURIComponent(getParameterByName('q'));
+    $searchInput.value(q);
+    execSearch(q);
+  }
 
-    // Get search results if q parameter is set in querystring
-    if (getParameterByName('q')) {
-        q = decodeURIComponent(getParameterByName('q'));
-        $searchInput.val(q);
-        execSearch(q);
-    }
-
-    // Get search results on submission of form
-    $(document).on("submit", $searchForm, function(e) {
-        e.preventDefault();
-        q = $searchInput.val();
-        execSearch(q);
-    });
+  // Get search results on submission of form
+  $(document).on('submit', $searchForm, function (e) {
+    e.preventDefault();
+    q = $searchInput.value();
+    execSearch(q);
+  });
 }
-
 
 /**
  * Executes search
- * @param {String} q 
+ * @param {String} q
  * @return null
  */
-function execSearch(q) {
-    if (q != '' || allowEmpty) {
-        if (showLoader) {
-            toggleLoadingClass();
-        }
-
-        getSearchResults(processData());
+function execSearch (q) {
+  if (q !== '' || allowEmpty) {
+    if (showLoader) {
+      toggleLoadingClass();
     }
-}
 
+    getSearchResults(processData());
+  }
+}
 
 /**
  * Toggles loading class on results and found string
  * @return null
  */
-function toggleLoadingClass() {
-    $resultsPlaceholder.toggleClass(loadingClass);
-    $foundContainer.toggleClass(loadingClass);
+function toggleLoadingClass () {
+  $resultsPlaceholder.classList(loadingClass);
+  $foundContainer.classList(loadingClass);
 }
-
 
 /**
  * Get Search results from JSON
- * @param {Function} callbackFunction 
+ * @param {Function} callbackFunction
  * @return null
  */
-function getSearchResults(callbackFunction) {
-    $.get(jsonFeedUrl, callbackFunction, 'json');
+function getSearchResults (callbackFunction) {
+  $.fetch(jsonFeedUrl, callbackFunction, 'json');
 }
-
 
 /**
  * Process search result data
  * @return null
  */
-function processData() {
-    $results = [];
-    
-    return function(data) {
-        
-        var resultsCount = 0,
-            results = "";
+function processData () {
+  return function (data) {
+    let resultsCount = 0;
+    let results = '';
 
-        $.each(data, function(index, item) {
-            // check if search term is in content or title 
-            if (item.search_omit != "true" && (item.content.toLowerCase().indexOf(q.toLowerCase()) > -1 || item.title.toLowerCase().indexOf(q.toLowerCase()) > -1)) {
-                var result = populateResultContent($resultTemplate.html(), item);
-                resultsCount++;
-                results += result;
-            }
-        });
+    $.forEach(data, function (_index, item) {
+      // check if search term is in content or title
+      if (
+        item.search_omit !== 'true' &&
+        (item.content.toLowerCase().indexOf(q.toLowerCase()) > -1 ||
+          item.title.toLowerCase().indexOf(q.toLowerCase()) > -1)
+      ) {
+        const result = populateResultContent($resultTemplate.innerHTML(), item);
+        resultsCount++;
+        results += result;
+      }
+    });
 
-        if (showLoader) {
-            toggleLoadingClass();
-        }
-
-        populateResultsString(resultsCount);
-        showSearchResults(results);
+    if (showLoader) {
+      toggleLoadingClass();
     }
-}
 
+    populateResultsString(resultsCount);
+    showSearchResults(results);
+  };
+}
 
 /**
  * Add search results to placeholder
  * @param {String} results
  * @return null
  */
-function showSearchResults(results) {
-    // Add results HTML to placeholder
-    $resultsPlaceholder.html(results);
+function showSearchResults (results) {
+  // Add results HTML to placeholder
+  $resultsPlaceholder.innerHTML(results);
 }
-
 
 /**
  * Add results content to item template
- * @param {String} html 
+ * @param {String} html
  * @param {object} item
  * @return {String} Populated HTML
  */
-function populateResultContent(html, item) {
-    html = injectContent(html, item.title, '##Title##');
-    html = injectContent(html, item.link, '##Url##');
-    html = injectContent(html, item.excerpt, '##Excerpt##');
-    html = injectContent(html, item.date, '##Date##');
-    return html;
+function populateResultContent (html, item) {
+  html = injectContent(html, item.title, '##Title##');
+  html = injectContent(html, item.link, '##Url##');
+  html = injectContent(html, item.excerpt, '##Excerpt##');
+  html = injectContent(html, item.date, '##Date##');
+  return html;
 }
-
 
 /**
  * Populates results string
- * @param {String} count 
+ * @param {String} count
  * @return null
  */
-function populateResultsString(count) {
-    $foundTerm.text(q);
-    $foundCount.text(count);
-    $foundContainer.show();
+function populateResultsString (count) {
+  $foundTerm.textContent(q);
+  $foundCount.textContent(count);
+  $foundContainer.textContent();
 }
 
-
-
-
- /* ==========================================================================
-    Helper functions
-    ========================================================================== */
-
+/* ==========================================================================
+   Helper functions
+   ========================================================================== */
 
 /**
  * Gets query string parameter - taken from http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
- * @param {String} name 
+ * @param {String} name
  * @return {String} parameter value
  */
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
-    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+function getParameterByName (name) {
+  const match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+  return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
-
 
 /**
  * Injects content into template using placeholder
  * @param {String} originalContent
  * @param {String} injection
- * @param {String} placeholder 
+ * @param {String} placeholder
  * @return {String} injected content
  */
-function injectContent(originalContent, injection, placeholder) {
-    var regex = new RegExp(placeholder, 'g');
-    return originalContent.replace(regex, injection);
+function injectContent (originalContent, injection, placeholder) {
+  const regex = new RegExp(placeholder, 'g');
+  return originalContent.replace(regex, injection);
 }
